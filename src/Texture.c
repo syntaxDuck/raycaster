7570498eaf0eb75125b13aa71f240a190969761c
syntaxDuck <kam.comer96@gmail.com>
2024-10-14 21:33:15 -0400

    Got texture loading working

--- src/Texture.c
+++ src/Texture.c
@@ -1,6 +1,6 @@
 #include "Texture.h"
 
-int *loadImageData(const char *filePath, int *width, int *height)
+Uint32 *loadImageData(const char *filePath, int *width, int *height)
 {
     // Load the image into an SDL_Surface
     SDL_Surface *surface = IMG_Load(filePath);
@@ -13,34 +13,42 @@ int *loadImageData(const char *filePath, int *width, int *height)
     // Store the width and height
     *width = surface->w;
     *height = surface->h;
-    SDL_ConvertSurfaceFormat(surface, SDL_PIXELFORMAT_RGBA8888, 0);
-    printf("Loaded surface: %dx%d, format: %d\n",
-           surface->w, surface->h, surface->format->format);
-    // Allocate an array of ints to hold the pixel data
-    int *pixelData = malloc(surface->w * surface->h * sizeof(int));
-    if (!pixelData)
+
+    // Convert surface to RGBA8888 format
+    SDL_Surface *converted_surface = SDL_ConvertSurfaceFormat(surface, SDL_PIXELFORMAT_RGBA8888, 0);
+    SDL_FreeSurface(surface); // Free the original surface
+    if (!converted_surface)
     {
-        fprintf(stderr, "Could not allocate memory for pixel data\n");
-        SDL_FreeSurface(surface);
+        fprintf(stderr, "Could not convert surface to RGBA8888: %s\n", SDL_GetError());
         return NULL;
     }
 
-    // Copy the pixel data from the surface to the int array
-    Uint32 *pixels = (Uint32 *)surface->pixels; // Cast pixels to Uint32*
-    int pitch = surface->pitch / sizeof(Uint32);
-    int pixel_num = 0;
-    for (int y = 0; y < surface->h; y++)
+    printf("Loaded surface: %dx%d, format: %d\n",
+           converted_surface->w, converted_surface->h, converted_surface->format->format);
+
+    // Allocate an array to hold the pixel data
+    Uint32 *pixelData = malloc(converted_surface->w * converted_surface->h * sizeof(Uint32));
+    if (!pixelData)
     {
-        for (int x = 0; x < surface->w; x++)
+        fprintf(stderr, "Could not allocate memory for pixel data\n");
+        SDL_FreeSurface(converted_surface);
+        return NULL;
+    }
+
+    // Copy the pixel data from the surface to the array
+    Uint32 *pixels = (Uint32 *)converted_surface->pixels;
+    for (int y = 0; y < converted_surface->h; y++)
+    {
+        for (int x = 0; x < converted_surface->w; x++)
         {
             // Get the pixel value and store it in the array
-            Uint32 pixel = pixels[y * surface->w + x];
-            pixelData[y * surface->w + x] = pixel;
+            Uint32 pixel = pixels[y * converted_surface->w + x];
+            pixelData[y * converted_surface->w + x] = pixel;
         }
     }
 
-    // Clean up the surface
-    SDL_FreeSurface(surface);
+    // Clean up the converted surface
+    SDL_FreeSurface(converted_surface);
 
     return pixelData;
 }
@@ -48,11 +56,31 @@ int *loadImageData(const char *filePath, int *width, int *height)
 Texture *createTextures()
 {
     // int textures[8][TEX_HEIGHT * TEX_WIDTH];
-    Texture *textures = malloc(sizeof(Texture) * 8);
+    Texture *textures = malloc(sizeof(Texture) * 11);
     int width, height;
 
-    // int *test = loadImageData("../assets/textures/pillar.png",
-    //                           &width, &height);
+    Uint32 *t0 = loadImageData("../assets/textures/barrel.png",
+                               &width, &height);
+    Uint32 *t1 = loadImageData("../assets/textures/bluestone.png",
+                               &width, &height);
+    Uint32 *t2 = loadImageData("../assets/textures/colorstone.png",
+                               &width, &height);
+    Uint32 *t3 = loadImageData("../assets/textures/eagle.png",
+                               &width, &height);
+    Uint32 *t4 = loadImageData("../assets/textures/greenlight2.png",
+                               &width, &height);
+    Uint32 *t5 = loadImageData("../assets/textures/greystone.png",
+                               &width, &height);
+    Uint32 *t6 = loadImageData("../assets/textures/mossy.png",
+                               &width, &height);
+    Uint32 *t7 = loadImageData("../assets/textures/pillar.png",
+                               &width, &height);
+    Uint32 *t8 = loadImageData("../assets/textures/purplestone.png",
+                               &width, &height);
+    Uint32 *t9 = loadImageData("../assets/textures/redbrick.png",
+                               &width, &height);
+    Uint32 *t10 = loadImageData("../assets/textures/wood.png",
+                                &width, &height);
 
     textures[0].pixels = malloc(sizeof(int) * (TEX_WIDTH * TEX_HEIGHT));
     textures[1].pixels = malloc(sizeof(int) * (TEX_WIDTH * TEX_HEIGHT));
@@ -62,23 +90,37 @@ Texture *createTextures()
     textures[5].pixels = malloc(sizeof(int) * (TEX_WIDTH * TEX_HEIGHT));
     textures[6].pixels = malloc(sizeof(int) * (TEX_WIDTH * TEX_HEIGHT));
     textures[7].pixels = malloc(sizeof(int) * (TEX_WIDTH * TEX_HEIGHT));
+    textures[8].pixels = malloc(sizeof(int) * (TEX_WIDTH * TEX_HEIGHT));
+    textures[9].pixels = malloc(sizeof(int) * (TEX_WIDTH * TEX_HEIGHT));
+    textures[10].pixels = malloc(sizeof(int) * (TEX_WIDTH * TEX_HEIGHT));
+
     for (int x = 0; x < TEX_WIDTH; x++)
     {
         for (int y = 0; y < TEX_HEIGHT; y++)
         {
-            int xorcolor = (x * 256 / TEX_WIDTH) ^ (y * 256 / TEX_HEIGHT);
+            // int xorcolor = (x * 256 / TEX_WIDTH) ^ (y * 256 / TEX_HEIGHT);
             // int xcolor = x * 256 / TEX_WIDTH;
-            int ycolor = y * 256 / TEX_HEIGHT;
-            int xycolor = y * 128 / TEX_HEIGHT + x * 128 / TEX_WIDTH;
-            // textures[0][TEX_WIDTH * y + x] = test[TEX_HEIGHT * y + x];
-            textures[0].pixels[TEX_WIDTH * y + x] = 65536 * 254 * (x != y && x != TEX_WIDTH - y); // flat red pixels with black cross
-            textures[1].pixels[TEX_WIDTH * y + x] = xycolor + 256 * xycolor + 65536 * xycolor;    // sloped greyscale
-            textures[2].pixels[TEX_WIDTH * y + x] = 256 * xycolor + 65536 * xycolor;              // sloped yellow gradient
-            textures[3].pixels[TEX_WIDTH * y + x] = xorcolor + 256 * xorcolor + 65536 * xorcolor; // xor greyscale
-            textures[4].pixels[TEX_WIDTH * y + x] = 256 * xorcolor;                               // xor green
-            textures[5].pixels[TEX_WIDTH * y + x] = 65536 * 192 * (x % 16 && y % 16);             // red bricks
-            textures[6].pixels[TEX_WIDTH * y + x] = 65536 * ycolor;                               // red gradient
-            textures[7].pixels[TEX_WIDTH * y + x] = 128 + 256 * 128 + 65536 * 128;                // flat grey pixels
+            // int ycolor = y * 256 / TEX_HEIGHT;
+            // int xycolor = y * 128 / TEX_HEIGHT + x * 128 / TEX_WIDTH;
+            // textures[0].pixels[TEX_WIDTH * y + x] = 65536 * 254 * (x != y && x != TEX_WIDTH - y); // flat red pixels with black cross
+            // textures[1].pixels[TEX_WIDTH * y + x] = xycolor + 256 * xycolor + 65536 * xycolor;    // sloped greyscale
+            textures[0].pixels[TEX_WIDTH * y + x] = t0[TEX_HEIGHT * y + x];
+            textures[1].pixels[TEX_WIDTH * y + x] = t1[TEX_HEIGHT * y + x];
+            textures[2].pixels[TEX_WIDTH * y + x] = t2[TEX_HEIGHT * y + x];
+            textures[3].pixels[TEX_WIDTH * y + x] = t3[TEX_HEIGHT * y + x];
+            textures[4].pixels[TEX_WIDTH * y + x] = t4[TEX_HEIGHT * y + x];
+            textures[5].pixels[TEX_WIDTH * y + x] = t5[TEX_HEIGHT * y + x];
+            textures[6].pixels[TEX_WIDTH * y + x] = t6[TEX_HEIGHT * y + x];
+            textures[7].pixels[TEX_WIDTH * y + x] = t7[TEX_HEIGHT * y + x];
+            textures[8].pixels[TEX_WIDTH * y + x] = t8[TEX_HEIGHT * y + x];
+            textures[9].pixels[TEX_WIDTH * y + x] = t9[TEX_HEIGHT * y + x];
+            textures[10].pixels[TEX_WIDTH * y + x] = t10[TEX_HEIGHT * y + x];
+            // textures[2].pixels[TEX_WIDTH * y + x] = 256 * xycolor + 65536 * xycolor;              // sloped yellow gradient
+            // textures[3].pixels[TEX_WIDTH * y + x] = xorcolor + 256 * xorcolor + 65536 * xorcolor; // xor greyscale
+            // textures[4].pixels[TEX_WIDTH * y + x] = 256 * xorcolor;                               // xor green
+            // textures[5].pixels[TEX_WIDTH * y + x] = 65536 * 192 * (x % 16 && y % 16);             // red bricks
+            // textures[6].pixels[TEX_WIDTH * y + x] = 65536 * ycolor;                               // red gradient
+            // textures[7].pixels[TEX_WIDTH * y + x] = 128 + 256 * 128 + 65536 * 128;                // flat grey pixels
         }
     }
 
